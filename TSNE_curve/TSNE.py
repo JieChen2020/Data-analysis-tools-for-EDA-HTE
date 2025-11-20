65b5b8a18748eaaec383052a27ec168495cb5fde
Jie Chen <120242820+JieChen2020@users.noreply.github.com>
2025-11-20 23:05:31 +0800

    Update TSNE.py

--- TSNE_curve/TSNE.py
+++ TSNE_curve/TSNE.py
@@ -3,9 +3,12 @@ from chemplot import Plotter
 import matplotlib.pyplot as plt
 
 
-data_BBBP = read_csv("filtered_molecules.csv")
+data_BBBP = read_csv("acid_sampled_file.csv")
+# data_BBBP = read_csv("amine_sampled_file.csv")
 cp = Plotter.from_smiles(data_BBBP["smiles"], target=data_BBBP["target"], target_type="C", sim_type="structural")
 a = cp.tsne(pca=False, random_state=20)
-a.to_csv('TSNE.csv', index=False)
+a.to_csv('acid_TSNE.csv', index=False)
+# a.to_csv('amine_TSNE.csv', index=False)
 cp.visualize_plot()
 plt.show()
+
